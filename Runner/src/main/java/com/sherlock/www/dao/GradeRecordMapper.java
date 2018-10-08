243c589e480b118355b390df1494806b177baace
sherlocklnixe <39846614+sherlocklnixe@users.noreply.github.com>
2018-10-08 10:37:23 +0800

    Revert "true"
    This reverts commit 0b52b60a2697b76d90def4c88361b0e34007dfc6.

--- Runner/src/main/java/com/sherlock/www/dao/GradeRecordMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sherlock.www.dao;
-
-import com.sherlock.www.entity.Graderecord;
-
-import tk.mybatis.mapper.common.Mapper;
-
-public interface GradeRecordMapper extends Mapper<Graderecord>{
-   
-}
--- Runner/src/main/java/com/sherlock/www/dao/MemberSpaceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.sherlock.www.dao;
-
-import com.sherlock.www.entity.Memberspace;
-
-public interface MemberSpaceMapper{
-	 Memberspace selectSpace(long id);
-	 void insertSpace(Memberspace space);
-}
